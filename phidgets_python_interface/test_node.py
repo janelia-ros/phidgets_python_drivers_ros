7b50206bfd5a71a8049e60781b493787068a43b0
Peter Polidoro <peterpolidoro@gmail.com>
2020-01-15 16:08:10 -0500

    Initial commit

---
+++ phidgets_python_interface/test_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from phidgets_python_interface.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+package_name = 'phidgets_python_interface'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Peter Polidoro',
+    author_email='peterpolidoro@gmail.com',
+    maintainer='Peter Polidoro',
+    maintainer_email='peterpolidoro@gmail.com',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Phidgets Python ROS interface.',
+    license='BSD',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'test_node = phidgets_python_interface.test_node:main'
+        ],
+    },
+)
